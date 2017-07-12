8aae6808d3ce006d6dd2f35d0a2447d27b51ba15
dcaled <dcaled@gmail.com>
2017-07-12 01:58:49 -0300

    Creating configuration file.

---
+++ config.py
@@ -0,0 +1,11 @@
+# Twitter Authentication Tokens.
+# Generate credentials via Twitter's Application Management:
+# https://apps.twitter.com/app/new
+
+consumer_key = "XxXxXxxXXXxxxxXXXxXX"
+consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
+access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
+access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+
+# Output path
+out_path = ''
